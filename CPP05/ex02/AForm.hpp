9bbafb89929ab71e89f3fae7a9a9a2f3838b5cad
Nicole Lehmeyer <136217617+NicoleLehmeyer@users.noreply.github.com>
2025-05-12 13:04:50 +0000

    Begun CPP05 ex02

---
+++ CPP05/ex02/AForm.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class AForm {
+	protected:
+		std::string	_name;
+		bool		_isSigned;
+		int			_signGrade;
+		int 		_executeGrade;
+
+	public:
+
+		AForm();
+		AForm(std::string const &name, int const signGrade, int const executeGrade);
+		AForm(AForm const &other);
+		AForm &operator=(AForm const &other);
+		virtual ~AForm();
+
+		std::string const &getName() const;
+		bool const &getSignedStatus() const;
+		int const &getSignGrade() const;
+		int const &getExecuteGrade() const;
+
+		void setName(std::string const &name);
+		void setSignedStatus(bool value);	
+		void setSignGrade(int const &signGrade);
+		void setExecuteGrade(int const &executeGrade);
+
+		void beSigned(Bureaucrat &aBureau);
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &os, AForm const &form_ref);
---
+++ CPP05/ex02/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include "AForm.hpp"
+
+class AForm;
+
+class Bureaucrat {
+	private:
+		std::string _name;
+		int			_grade;
+	
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string const &name, int const grade);
+		Bureaucrat(Bureaucrat const &other);
+		Bureaucrat &operator=(Bureaucrat const &other);
+		~Bureaucrat();
+
+		std::string const &getName() const;
+		int const &getGrade() const;
+		
+		void setName(std::string const &name);
+		void setGrade(int const &grade);
+
+		void incrementGrade();
+		void decrementGrade();
+
+		void signForm(AForm &form_ref);
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+
+};
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &aBureau);
