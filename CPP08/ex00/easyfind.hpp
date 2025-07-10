375318bfe5adcf66b10d0e91f0087297cbe957b8
Nicole Lehmeyer <nicole.lehmeyer@icloud.com>
2025-07-10 19:27:25 +0930

    V2 CPP08 initial files

---
+++ CPP08/ex00/easyfind.hpp
@@ -0,0 +1,10 @@
+
+
+// Write function easyfind() that accepts a type 'T'
+// It takes two params, one type T, another integer
+
+// T is a container of integers
+// Function must find first occurence of second parameter in first parameter
+
+// NO OCCURRENCE - Throw exception or return error of choice
+// TIP: Analyse how standard containers behave
---
+++ CPP08/ex01/Span.cpp
@@ -0,0 +1,13 @@
+
+
+// Create 'Span' class that stores a maximum of N integers.
+// N is an unsigned int variable & only parameter passed to the constructor
+
+// Member functions:
+//	addNumber() - add single number to the Span.
+//		- It will be used in order to fill it
+//		- Any attempt to add a new element if there are already N elements should throw an exception
+
+//	shortest/longestSpan() - find and return the shortest span between all the numbers stored
+
+//	
