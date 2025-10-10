9747126a519638b09b69192ff17dd1f65a748efa
Nicole Lehmeyer <nicole.lehmeyer@icloud.com>
2025-10-10 17:52:57 +1030

    Working on CPP08 ex01

--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -2,9 +2,14 @@
 class Span {
 
 	public:
-		addNumber();
-		shortestSpan();
-		longestSpan();
+		Span(unsigned int N);
+		Span(const Span& copy);
+		Span& operator=(const Span& other);
+		~Span();
+	
+		void addNumber();
+		int shortestSpan();
+		int longestSpan();
 
 	private:
 
--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -10,11 +10,12 @@ int main()
 	sp.addNumber(9);
 	sp.addNumber(11);
 
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	std::cout << "Shortest Span: " << sp.shortestSpan() << std::endl;
+	std::cout << "Longest Span: " << sp.longestSpan() << std::endl;
 
 	return 0;
 }
 
 //should output: 2, 14
-//Fill Span using a range of itertors. Make thousands of calls to addNumber with one member function to make one call.
+//Fill Span using a range of iterators.
+//Make thousands of calls to addNumber with one member function to make one call.
